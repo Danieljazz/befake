8681ddd75052c4f97ad3b366256218b66d9201ac
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-01-19 20:55:18 +0100

    Routes and controllers preconditions

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
-import Express, { json } from "express";
-const app = Express();
+import express from "express";
+import usersRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+const app = express();
+app.use(express.json());
+app.use("/api/v1/users", usersRoutes);
+app.use("/api/v1/auth", authRoutes);
 app.listen("8080", () => {
   console.log("backend run on port 8080");
 });
---
+++ api/routes/auth.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { register, login, logout } from "../controllers/auth.js";
+const router = express.Router();
+router.post("/login", login);
+router.post("/register", register);
+router.post("/logout", logout); // useless?
+
+export default router;
