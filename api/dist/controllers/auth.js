642d60e944bf6dc90eed3e8fcabafd04cc00a05d
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-01-24 00:10:38 +0100

    Added register

--- api/dist/controllers/auth.js
+++ api/dist/controllers/auth.js
@@ -1,13 +1,47 @@
 import { db } from "../connect.js";
-export const login = (req, res) => {
-    const q = "SELECT * FROM users";
-    db.query(q, (err, data) => {
-        if (err) {
+import cryptoJS from "crypto-js";
+export const register = (req, res) => {
+    const q = "SELECT * FROM users WHERE username = ?";
+    db.query(q, [req.body.username], (err, data) => {
+        if (err)
             return res.status(500).json(err);
-        }
-        return res.status(200).json(data);
+        if (data.length)
+            return res.status(409).json("User already  exists");
+        const hashedPassword = cryptoJS.AES.encrypt(req.body.password, process.env.n5jYy70iY);
+        const iq = "INSERT INTO users (`username`, `mail`, `name`, `surname`, `password`) VALUE (?)";
+        const values = [
+            req.body.username,
+            req.body.mail,
+            req.body.name,
+            req.body.surname,
+            hashedPassword,
+        ];
+        db.query(iq, [values], (err, data) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+            if (data) {
+                return res.status(200).json("User registered properly");
+            }
+        });
     });
 };
-export const register = (req, res) => { };
+export const login = (req, res) => {
+    const q = "SELECT * FROM users WHERE username = ?";
+    db.query(q, [req.body.username], (err, data) => {
+        if (err)
+            return res.status(403).json("Wrong credentials");
+        if (data.length === 0)
+            return res.status(404).json("User not found");
+        const hashedPassword = cryptoJS.AES.decrypt(data[0].password, process.env.n5jYy70iY);
+        const orgPass = hashedPassword.toString(cryptoJS.enc.Utf8);
+        if (orgPass === req.body.password) {
+            return res.status(200).json("Access granted");
+        }
+        else {
+            return res.status(403).json("Wrong credentials");
+        }
+    });
+};
 export const logout = (req, res) => { };
 //# sourceMappingURL=auth.js.map
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { register, login, logout } from "../controllers/auth.js";
 const router = express.Router();
-router.get("/login", login);
 router.post("/register", register);
+router.post("/login", login);
 router.post("/logout", logout); // useless?
 
 export default router;
