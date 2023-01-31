7e3f491b8d1aa8d64e9dc4296202afc07bd5a852
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-01-31 20:46:11 +0100

    Added posibility to add comment

--- api/src/controllers/comments.ts
+++ api/src/controllers/comments.ts
@@ -1,10 +1,32 @@
+import moment from "moment";
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
 export const getComments = (req, res) => {
-  const q = `SELECT c.*, u.id AS commentUserId, name, surname, profilePhoto FROM comments AS c JOIN users AS u ON(c.commentPostId = ? AND c.commentUserId = u.id)`;
+  const q = `SELECT c.*, u.id AS commentUserId, name, surname, 
+              profilePhoto FROM comments AS c JOIN users AS u
+              ON(c.commentPostId = ? AND c.commentUserId = u.id) ORDER BY c.createdAt DESC`;
   const postId = req.query.postId;
   db.query(q, [postId], (err, data) => {
     if (err) return res.status(500).json(err);
     return res.status(200).json(data);
   });
 };
-export const createComment = () => {};
+
+export const createComment = (req, res) => {
+  const token = req.cookies.accessToken;
+  jwt.verify(token, process.env.key, (err, userInfo) => {
+    if (err) return res.status(403).json("Token not valid");
+    const q =
+      "INSERT INTO comments (commentPostId, commentUserId, commentContent, createdAt) VALUES (?)";
+    const value = [
+      req.body.commentPostId,
+      userInfo.id,
+      req.body.commentContent,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+    ];
+    db.query(q, [value], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data) return res.status(200).json("comment created");
+    });
+  });
+};
--- client/src/components/CommentsSection/CommentSection.tsx
+++ client/src/components/CommentsSection/CommentSection.tsx
@@ -4,7 +4,8 @@ import { useContext } from "react";
 import { Comment } from "../../components/Comment/Comment";
 import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 import { makeRequest } from "../../axiosRequest";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 type CommentsProps = {
   comments?: [
     {
@@ -21,19 +22,45 @@ type CommentsProps = {
 
 export const CommentsSection = ({ postId }: string) => {
   const { user } = useContext(AuthContext);
+  const [commentDesc, setCommentDesc] = useState("");
+  const queryClient = useQueryClient();
   const { isLoading, error, data } = useQuery(["comments"], () =>
     makeRequest.get(`/comments?postId=${postId}`).then((res) => {
       return res.data;
     })
   );
+  const commentMutation = useMutation(
+    (newComment) => {
+      makeRequest.post("/comments", newComment);
+    },
+    {
+      onSuccess: () => {
+        setCommentDesc("");
+        return queryClient.invalidateQueries(["comments"]);
+      },
+    }
+  );
+  const createComment = () => {
+    commentMutation.mutate({
+      commentPostId: postId,
+      commentContent: commentDesc,
+    });
+  };
   return (
     <div className="comments-section">
       <div className="new-comment">
         <div className="user">
           <img src={user.profilePhoto} alt="" />
         </div>
-        <input type="text" placeholder="Type comment" />
-        <button>Send</button>
+        <input
+          type="text"
+          placeholder="Type comment"
+          onChange={(e) => setCommentDesc(e.target.value)}
+          value={commentDesc}
+        />
+        <button style={{ cursor: "pointer" }} onClick={createComment}>
+          Send
+        </button>
       </div>
       {data &&
         data.map((comment) => <Comment key={comment.id} comment={comment} />)}
