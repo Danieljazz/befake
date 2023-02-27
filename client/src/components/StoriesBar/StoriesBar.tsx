9f833d3b972ce7d5cfb1968b3d0ae208fd05f1a3
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-02-27 22:35:29 +0100

    Added story bar slider

--- client/src/components/StoriesBar/StoriesBar.tsx
+++ client/src/components/StoriesBar/StoriesBar.tsx
@@ -1,36 +1,66 @@
 import "./storiesbar.scss";
 import { Story } from "../Story/Story";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { useState } from "react";
 export const StoriesBar = () => {
+  let storiesLen = 9;
+  let possibleMove = storiesLen - 7;
+  const [move, setMove] = useState<number>(1);
+  const moveStories = (direction: string) => {
+    if (direction === "right") {
+      setMove((prev) => (prev = prev + 1));
+    } else {
+      setMove((prev) => (prev = prev - 1));
+    }
+  };
   return (
     <div className="stories-bar">
-      <Story
-        name="Jane Doe"
-        photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <Story
-        name="Jane Doe"
-        photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <Story
-        name="Jane Doe"
-        photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <Story
-        name="Jane Doe"
-        photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <Story
-        name="Jane Doe"
-        photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />{" "}
-      <Story
-        name="Jane Doe"
-        photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <Story
-        name="Jane Doe"
-        photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
+      <div
+        className="stories"
+        style={{
+          transform: `translateX(${160 - 155 * move}px)`,
+        }}
+      >
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />{" "}
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <Story
+          name="Jane Doe"
+          photo="https://images.pexels.com/photos/3533228/pexels-photo-3533228.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+      </div>{" "}
+      <button className="left" onClick={() => moveStories("left")}>
+        <ArrowBackIosIcon />
+      </button>
+      <button className="right" onClick={() => moveStories("right")}>
+        <ArrowForwardIosIcon />
+      </button>
     </div>
   );
 };
