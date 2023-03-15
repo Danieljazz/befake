c7d3e91e48bcb96218a24c31c8e8b8adbb7b0aa3
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-03-15 21:00:34 +0100

    Update query

--- api/src/controllers/chats.ts
+++ api/src/controllers/chats.ts
@@ -7,10 +7,10 @@ export const userMessages = (req, res) => {
   jwt.verify(accessToken, process.env.key, (error, senderId) => {
     if (error) return res.status(403).json("Wrong credentials");
     const q = `SELECT c.*, u.id AS reciverId, name, surname, 
-                profilePhoto FROM chats AS c JOIN users AS u 
-                ON(u.id = c.reciverId) WHERE 
-                senderId = ? AND reciverId = ?
-                ORDER BY createdAt DESC`;
+profilePhoto FROM chats AS c JOIN users AS u 
+ON(u.id = c.reciverId) WHERE 
+senderId = ? AND reciverId = ? 
+ORDER BY createdAt DESC`;
     //   `SELECT c.*, u.id AS userId, name, surname, profilePhoto FROM chats AS c JOIN users as u ON(user.id = reciverId) `
     db.query(q, [senderId.id, reciverId, reciverId], (error, data) => {
       if (error) return res.status(500).json(error);
