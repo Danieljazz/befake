9123d2aa4cda65df37e03e3504cd90136c2d053a
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-03-15 19:58:53 +0100

    up

--- api/src/controllers/chats.ts
+++ api/src/controllers/chats.ts
@@ -1,7 +1,7 @@
 import { db } from "../connect.js";
 
 export const userMessages = (req, res) => {
-  const param = req.query.id;
+  const param = req.params.id;
   return res.status(200).json(param);
 };
 export const createMessage = (req, res) => {};
--- api/src/routes/chats.ts
+++ api/src/routes/chats.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { userMessages, createMessage } from "../controllers/chats.js";
 const router = express.Router();
 
-router.get("/chat/:id", userMessages);
-router.post("/chat/:id", createMessage);
+router.get("/:id", userMessages);
+router.post("/:id", createMessage);
 
 export default router;
