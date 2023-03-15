e52aa663375ace4b2aa15563ef842362235dffb6
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-03-15 19:51:34 +0100

    up

--- api/src/controllers/chats.ts
+++ api/src/controllers/chats.ts
@@ -1,7 +1,7 @@
 import { db } from "../connect.js";
 
 export const userMessages = (req, res) => {
-  const param = req.query.chatsId;
+  const param = req.query.id;
   return res.status(200).json(param);
 };
 export const createMessage = (req, res) => {};
