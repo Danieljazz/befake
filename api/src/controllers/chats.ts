c6884daa5996de970b30918ff16fb3f7ebf1c7e8
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-03-15 20:36:59 +0100

    Upd

--- api/src/controllers/chats.ts
+++ api/src/controllers/chats.ts
@@ -8,7 +8,7 @@ export const userMessages = (req, res) => {
     if (error) return res.status(403).json("Wrong credentials");
     const q = `SELECT c.*, u.id AS reciverId, name, surname, profilePhoto FROM chats AS c JOIN users AS u WHERE senderId = ? AND reciverId = ? LEFT JOIN users ON(u.id = ?) ORDER BY createdAt DESC`;
     //   `SELECT c.*, u.id AS userId, name, surname, profilePhoto FROM chats AS c JOIN users as u ON(user.id = reciverId) `
-    db.query(q, [senderId, reciverId, reciverId], (error, data) => {
+    db.query(q, [senderId.id, reciverId, reciverId], (error, data) => {
       if (error) return res.status(500).json(error);
       return res.status(200).json(data);
     });
