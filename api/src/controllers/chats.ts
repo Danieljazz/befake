a98ca9ef33e77bfd2d1d4ed2f1dfeaa9295c222b
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-03-15 21:18:42 +0100

    Query update

--- api/src/controllers/chats.ts
+++ api/src/controllers/chats.ts
@@ -6,13 +6,13 @@ export const userMessages = (req, res) => {
   const accessToken = req.cookies.accessToken;
   jwt.verify(accessToken, process.env.key, (error, senderId) => {
     if (error) return res.status(403).json("Wrong credentials");
-    const q = `SELECT c.*, u.id AS reciverId, name, surname, 
-profilePhoto FROM chats AS c JOIN users AS u 
-ON(u.id = c.reciverId) WHERE 
-c.senderId = ? AND c.reciverId = ? 
-ORDER BY c.createdAt DESC`;
-    //   `SELECT c.*, u.id AS userId, name, surname, profilePhoto FROM chats AS c JOIN users as u ON(user.id = reciverId) `
-    db.query(q, [senderId.id, reciverId, reciverId], (error, data) => {
+    const q = `SELECT c.*, u.id AS receiverId, name, surname, profilePhoto FROM chats AS c JOIN users AS u ON(u.id = c.receiverId) WHERE c.senderId = ? AND c.receiverId = ? ORDER BY c.createdAt DESC`;
+    //     //   `SELECT c.*, u.id AS reciverId, name, surname,
+    // profilePhoto FROM chats AS c JOIN users AS u
+    // ON(u.id = c.reciverId) WHERE
+    // c.senderId = ? AND c.reciverId = ?
+    // ORDER BY c.createdAt DESC`;
+    db.query(q, [senderId.id, reciverId], (error, data) => {
       if (error) return res.status(500).json(error);
       return res.status(200).json(data);
     });
