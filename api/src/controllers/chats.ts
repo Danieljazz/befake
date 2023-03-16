f8da1e6f668347ee113eca7e6bee34d0e51eb02a
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-03-16 19:45:58 +0100

    Create message update

--- api/src/controllers/chats.ts
+++ api/src/controllers/chats.ts
@@ -1,14 +1,13 @@
-import moment from "moment";
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
 
 export const userMessages = (req, res) => {
-  const reciverId = req.params.reciverId;
+  const receiverId = req.params.receiverId;
   const accessToken = req.cookies.accessToken;
   jwt.verify(accessToken, process.env.key, (error, senderId) => {
     if (error) return res.status(403).json("Wrong credentials");
     const q = `SELECT c.*, u.id AS receiverId, name, surname, profilePhoto FROM chats AS c JOIN users AS u ON(u.id = c.receiverId) WHERE c.senderId = ? AND c.receiverId = ? ORDER BY c.createdAt DESC`;
-    db.query(q, [senderId.id, reciverId], (error, data) => {
+    db.query(q, [senderId.id, receiverId], (error, data) => {
       if (error) return res.status(500).json(error);
       return res.status(200).json(data);
     });
--- api/src/routes/chats.ts
+++ api/src/routes/chats.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { userMessages, createMessage } from "../controllers/chats.js";
 const router = express.Router();
 
-router.get("/:reciverId", userMessages);
-router.post("/:reciverId", createMessage);
+router.get("/:receiverId", userMessages);
+router.post("/:receiverId", createMessage);
 
 export default router;
