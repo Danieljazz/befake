83295f83b5be36b928b0f02d33acdd7f4378cc30
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-05-04 20:10:53 +0200

    Created read endpoint for notifications

--- api/src/controllers/notifications.ts
+++ api/src/controllers/notifications.ts
@@ -12,4 +12,17 @@ export const getNotifications = (req, res) => {
     });
   });
 };
-export const readNotification = () => {};
+export const readNotification = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) res.status(403).json("User not logged in");
+  jwt.verify(token, process.env.key, (err, user) => {
+    if (err) return res.status(403).json("Wrong credentials");
+    const q = `UPDATE notifications SET notif_read = ? WHERE id=?`;
+    db.query(q, [req.body.notif_read, req.body.id], (err, data) => {
+      if (err) return res.status(500).json("Smth went wrong");
+      return res
+        .status(200)
+        .json(`Notification read status changed to ${req.body.notif_read}`);
+    });
+  });
+};
