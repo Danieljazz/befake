a38971710eb8e66c2ffa5e3fa37610ca1e69d427
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2023-05-11 19:54:00 +0200

    Finished friends request

--- api/src/controllers/stories.ts
+++ api/src/controllers/stories.ts
@@ -5,7 +5,7 @@ export const getStories = (req, res) => {
   const token = req.cookies.accessToken;
   jwt.verify(token, process.env.key, (err, userInfo) => {
     if (err) return res.status(403).json(err);
-    const q = `SELECT s.*, u.id AS userId, name, surname FROM stories AS s JOIN users AS u ON(s.storyUserId = u.id) LEFT JOIN relationships AS r ON(s.storyUserId = r.followedUserId) WHERE r.followingUserId = ? OR s.storyUserId = ? ORDER BY createdAt DESC`;
+    const q = `SELECT s.*, u.id AS userId, name, surname FROM stories AS s LEFT JOIN users AS u ON(s.storyUserId = u.id) LEFT JOIN relationships AS r ON(s.storyUserId = r.followedUserId) WHERE r.followingUserId = ? OR s.storyUserId = ? ORDER BY createdAt DESC`;
     db.query(q, [userInfo.id, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
       if (data.length > 0) {
--- client/src/components/UserSuggestions/UserSuggestions.tsx
+++ client/src/components/UserSuggestions/UserSuggestions.tsx
@@ -1,65 +1,71 @@
-import { useQuery } from "@tanstack/react-query";
-import { makeRequest } from "axiosRequest";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axiosRequest";
+
+import { AuthContext, UserContextType } from "../../context/authContext";
+import { useContext } from "react";
 
 const UserSuggestions = () => {
   const { isError, isLoading, data } = useQuery(["recomendedUsers"], () =>
-    makeRequest.get("")
+    makeRequest.get("/relationships/friendRequest").then((res) => res.data)
   );
+  const { isLoading: rIsLoading, data: relationshipData } = useQuery(
+    ["relationships"],
+    () =>
+      makeRequest
+        .get(`/relationships?userId=${8}`)
+        .then((res) => {
+          return res.data;
+        })
+        .catch((err) => {
+          return err;
+        }),
+    { initialData: [] }
+  );
+  const queryClient = useQueryClient();
+  const followMutation = useMutation(
+    ({ followed, userId }: { followed: boolean; userId: number }) => {
+      if (followed) {
+        return makeRequest.delete(`/relationships?followedUserId=${userId}`);
+      } else {
+        return makeRequest.post(`/relationships`, { followedUserId: userId });
+      }
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["relationships"]);
+      },
+    }
+  );
+
+  const userFriends = relationshipData.map((user: UserContextType) => user.id);
+
+  const handleFollow = (e: React.FormEvent) => {
+    const target = e.target as HTMLInputElement;
+    const userId: number = Number(target.value);
+    followMutation.mutate({ followed: userFriends.includes(userId), userId });
+  };
   return (
     <div className="section">
       <span>Suggestions for you</span>
-      <div className="event">
-        <div className="user">
-          <img
-            src="https://images.pexels.com/photos/3394658/pexels-photo-3394658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-          />
-          <span>Jane Doe</span>
+      {data?.map((suggestedFriend: UserContextType) => (
+        <div className="event">
+          <div className="user">
+            <img src={suggestedFriend.profilePhoto} alt="" />
+            <span>{`${suggestedFriend.name} ${suggestedFriend.surname} `}</span>
+          </div>
+          <div className="buttons">
+            <button
+              value={suggestedFriend.id}
+              onClick={handleFollow}
+              style={{ backgroundColor: "#00ff75", cursor: "pointer" }}
+            >
+              {!userFriends?.includes(suggestedFriend.id)
+                ? "Follow"
+                : "Unfollow"}
+            </button>
+          </div>
         </div>
-        <div className="buttons">
-          <button style={{ backgroundColor: "#00ff75" }}>Accept</button>
-          <button style={{ backgroundColor: "#ff5d5d" }}>Dismiss</button>
-        </div>
-      </div>
-      <div className="event">
-        <div className="user">
-          <img
-            src="https://images.pexels.com/photos/3394658/pexels-photo-3394658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-          />
-          <span>Jane Doe</span>
-        </div>
-        <div className="buttons">
-          <button style={{ backgroundColor: "#00ff75" }}>Accept</button>
-          <button style={{ backgroundColor: "#ff5d5d" }}>Dismiss</button>
-        </div>
-      </div>
-      <div className="event">
-        <div className="user">
-          <img
-            src="https://images.pexels.com/photos/3394658/pexels-photo-3394658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-          />
-          <span>Jane Doe</span>
-        </div>
-        <div className="buttons">
-          <button style={{ backgroundColor: "#00ff75" }}>Accept</button>
-          <button style={{ backgroundColor: "#ff5d5d" }}>Dismiss</button>
-        </div>
-      </div>
-      <div className="event">
-        <div className="user">
-          <img
-            src="https://images.pexels.com/photos/3394658/pexels-photo-3394658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-          />
-          <span>Jane Doe</span>
-        </div>
-        <div className="buttons">
-          <button style={{ backgroundColor: "#00ff75" }}>Accept</button>
-          <button style={{ backgroundColor: "#ff5d5d" }}>Dismiss</button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
